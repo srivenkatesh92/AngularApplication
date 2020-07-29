fc4e6617baf86709b54f498137d5d9cae551b9d9
srivenkatesh <srivenkatesh2@gmail.com>
2020-07-29 19:17:04 +0530

    header footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from 'src/dashboard/dashboard.component';
 
 //components
 
 
 const routes: Routes = [
   //{path:'', redirectTo:'dashboard',pathMatch:'full'},
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
   
   {path:'**', redirectTo:'dashboard'}
 ];
