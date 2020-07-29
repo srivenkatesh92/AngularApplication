36a782a789e8cb15ff24c69c4fcc3fe21873fa81
srivenkatesh <srivenkatesh2@gmail.com>
2020-07-29 19:01:31 +0530

    remove modules -- restructure the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { LayoutModule } from './Layout/layout.module';
+
 import { AppComponent } from './app.component';
 import {DashboardModule} from '../dashboard/dashboard.module';
 
